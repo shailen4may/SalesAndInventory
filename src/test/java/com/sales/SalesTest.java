9de5af78e3af44214de9e70c87754a649681c544
My PC <My PC@DESKTOP-T56DNVO>
2024-01-24 18:08:39 +0530

    sales 2

---
+++ src/test/java/com/sales/SalesTest.java
@@ -0,0 +1,13 @@
+package com.sales;
+
+import org.testng.annotations.Test;
+
+public class SalesTest {
+	@Test
+	public void saleTest()
+	{
+		System.out.println("--sales successfull--");
+	}
+	
+
+}
